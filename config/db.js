72432502adc105f35acd3a8c3b1f8091e9789541
Mostafa Mohamed <117801510+Mostafa8518@users.noreply.github.com>
2023-02-14 00:23:49 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
+    const conn = await mongoose.connect("mongodb://mostafa:1989@ac-egbungw-shard-00-00.iqaskof.mongodb.net:27017,ac-egbungw-shard-00-01.iqaskof.mongodb.net:27017,ac-egbungw-shard-00-02.iqaskof.mongodb.net:27017/?ssl=true&replicaSet=atlas-jxqbg0-shard-0&authSource=admin&retryWrites=true&w=majority", {
      
     } ,mongoose.set('strictQuery', false));
 
