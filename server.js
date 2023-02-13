bd0b4213793c57f8645061d73a33fac11cb4c98f
Mostafa Mohamed <117801510+Mostafa8518@users.noreply.github.com>
2023-02-13 23:10:28 +0200

    first

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import bodyParser from 'body-parser';
+import cors from "cors";
+import connectDB from "./config/db.js";
+import articleRoutes from "./Routes/articleRoutes.js";
+import userRoutes from "./Routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import uploadRoutes from "./Routes/uploadRoutes.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/article", articleRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+}
+else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
