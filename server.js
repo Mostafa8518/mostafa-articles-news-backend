a31005a0092fa1a570e3847562fa76ea91b4346b
Mostafa Mohamed <117801510+Mostafa8518@users.noreply.github.com>
2023-02-15 00:41:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'https://whatsnow2.firebaseapp.com/build/uploads')))
+  app.use(express.static(path.join(__dirname, 'https://whatsnow2.web.app/build/uploads')))
 
-  app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname,https:'//whatsnow2.firebaseapp.com/','build','index.html')))
+  app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname,'https://whatsnow2.web.app/','build','index.html')))
 }
 else {
   app.get("/", (req, res) => {
