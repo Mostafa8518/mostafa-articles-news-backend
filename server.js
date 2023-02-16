c6d848ca4ea2dca703924922187cb95ef5edfca5
Mostafa Mohamed <117801510+Mostafa8518@users.noreply.github.com>
2023-02-16 20:07:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/upload", uploadRoutes);
 
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use("https://whatsnow-backend.onrender.com/uploads", express.static(path.join(__dirname, "/uploads")));
 
 
   app.get("/", (req, res) => {
