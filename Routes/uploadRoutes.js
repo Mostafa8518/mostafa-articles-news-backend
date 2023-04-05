b4f81f7791984a30bfac4451e29475b337c1a723
Mostafa Mohamed <117801510+Mostafa8518@users.noreply.github.com>
2023-04-06 00:10:57 +0200

    Update uploadRoutes.js

--- Routes/uploadRoutes.js
+++ Routes/uploadRoutes.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 router.post("/", upload.single("image"), (req, res) => {
-  res.send(`https://whatsnow-backend.onrender.com/${req.file.path}`);
+  res.send(`/${req.file.path}`);
 });
 
 export default router;
