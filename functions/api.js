c6250a488ef131e364e2f3bee684697b6a71d3ff
Mostafa Mohamed <117801510+Mostafa8518@users.noreply.github.com>
2023-04-06 13:04:17 +0200

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -27,7 +27,7 @@ app.use("/.netlify/functions/api/user", userRoutes);
 app.use("/.netlify/functions/api/upload", uploadRoutes);
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use("/../uploads", express.static(path.join(__dirname, "/../uploads")));
 
 
   app.get("/", (req, res) => {
