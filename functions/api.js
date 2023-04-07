240817ed27ffd85910cd255544bbf0355bc12def
Mostafa Mohamed <117801510+Mostafa8518@users.noreply.github.com>
2023-04-07 10:09:29 +0200

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -3,19 +3,30 @@ import path from "path";
 import dotenv from "dotenv";
 import bodyParser from 'body-parser';
 import cors from "cors";
-import connectDB from "../config/db.js";
+
 import articleRoutes from "../Routes/articleRoutes.js";
 import userRoutes from "../Routes/userRoutes.js";
 import { notFound, errorHandler } from "../middleware/errorMiddleware.js";
 import uploadRoutes from "../Routes/uploadRoutes.js";
 import serverless from "serverless-http";
-
+import mongoose from "mongoose";
 
 dotenv.config();
-
-connectDB();
-
 const app = express();
+const db = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb://mostafa:1989@ac-egbungw-shard-00-00.iqaskof.mongodb.net:27017,ac-egbungw-shard-00-01.iqaskof.mongodb.net:27017,ac-egbungw-shard-00-02.iqaskof.mongodb.net:27017/?ssl=true&replicaSet=atlas-jxqbg0-shard-0&authSource=admin&retryWrites=true&w=majority", {
+     
+    } ,mongoose.set('strictQuery', false));
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -27,7 +38,7 @@ app.use("/.netlify/functions/api/user", userRoutes);
 app.use("/.netlify/functions/api/upload", uploadRoutes);
 
 
-app.use("/../uploads", express.static(path.join(__dirname, "/../uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 
   app.get("/", (req, res) => {
